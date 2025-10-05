bc11a1f552f963ee9c736e4761bc45c61819c914
shashikumar <shashikumar.k@proverne.com>
2025-10-06 00:24:37 +0530

    Added fields for searching rides.

--- src/app/pages/book-ride/book-ride.ts
+++ src/app/pages/book-ride/book-ride.ts
@@ -3,16 +3,33 @@ import { Ride, RideTile } from '../../components/ride-tile/ride-tile';
 import { CommonModule } from '@angular/common';
 import { TransportService } from '../../services/transport-service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-book-ride',
-  imports: [RideTile,CommonModule],
+  imports: [RideTile,CommonModule,MatFormFieldModule,ReactiveFormsModule,FormsModule,MatSelectModule,MatOptionModule,MatButtonModule,MatInputModule],
   templateUrl: './book-ride.html',
   styleUrl: './book-ride.css'
 })
 export class BookRide implements OnInit {
+onSearch() {
+// throw new Error('Method not implemented.');
+}
   private transportService = inject(TransportService);
   private destroyRef = inject(DestroyRef);
+  private fb = inject(FormBuilder);
+
+  vehicleTypes = ['Bike', 'Car'];
+  searchRideForm: FormGroup=this.fb.group({
+      vehicleType: [this.vehicleTypes, Validators.required],
+      time: '',
+    });
+
   rides:Ride[] =[];
   bookedrides:Ride[] =[];
   ngOnInit(): void {
