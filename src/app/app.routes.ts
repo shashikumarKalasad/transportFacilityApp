61e9f2cdc81279709bcff273b45101221113f551
shashikumar <shashikumar.k@proverne.com>
2025-10-05 19:03:37 +0530

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { BookRide } from './pages/book-ride/book-ride';
+import { AddViewRides } from './pages/add-view-rides/add-view-rides';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'book-ride',
+        component: BookRide,
+        title: 'Available rides',
+    },
+    {
+        path: 'add-view-rides',
+        component: AddViewRides,
+        title: 'View Rides',
+    },
+];
+;
