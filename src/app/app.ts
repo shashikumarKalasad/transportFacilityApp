c42b9ecd9e61b08d2c6118baa276dbb8f79db2fd
shashikumar <shashikumar.k@proverne.com>
2025-10-05 20:32:08 +0530

    Added navigation sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, FormsModule, MatButtonModule, MatListModule],
+
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('transport-facility');
+  opened = true;
 }
